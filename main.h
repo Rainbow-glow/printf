3af868e5380f31f1fc9fb43fb2680a7049119fb9
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-10-18 13:53:07 -0700

    print int and dec

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -11,5 +12,6 @@ int printf_char(va_list args)
 int _strlen(char *s);
 int _strlenconst(const char *s);
 int printf_string(va_list args);
+int printf_bin(va_list args);
 
 #endif
